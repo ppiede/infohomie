d4f3afec4f69174a4efce78a4e0fe0b6fbb64e36
lkoehl <koehl.lukas@gmx.de>
2021-02-15 11:21:13 +0100

    Decision tree is now rendering properly

--- src/decision-tree-visualizer/index.js
+++ src/decision-tree-visualizer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Vertex from "./Vertex";
+import DataEntry from "../components/DataEntry";
 
 function DecisionTree({ data, criteria, usedCriteria }) {
   const sortData = (data, level) => {
@@ -26,7 +27,39 @@ function DecisionTree({ data, criteria, usedCriteria }) {
     return [sort1 ? sort1 : group1, sort2 ? sort2 : group2];
   };
 
-  const sortedData = React.useMemo(() => sortData(data, 0), []);
+  const sortData1 = (data, level) => {
+    const key = usedCriteria[level];
+    const group1 = [];
+    const group2 = [];
+
+    if (!key) {
+      return;
+    }
+
+    for (let entry of data) {
+      if (entry.criteria[key] === criteria[key].options[0]) {
+        group1.push(entry);
+      } else {
+        group2.push(entry);
+      }
+    }
+    let result = [];
+
+    sortData1(group1, level + 1)
+      ? result.push(...sortData1(group1, level + 1))
+      : result.push(group1);
+    sortData1(group2, level + 1)
+      ? result.push(...sortData1(group2, level + 1))
+      : result.push(group2);
+
+    return result;
+  };
+
+  const sortedData = React.useMemo(() => sortData1(data, 0), [
+    data,
+    criteria,
+    usedCriteria,
+  ]);
 
   const renderVertices = (amount, key) => {
     const result = [];
@@ -65,6 +98,41 @@ function DecisionTree({ data, criteria, usedCriteria }) {
           </div>
         );
       })}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          padding: "8px",
+          justifyContent: "space-around",
+        }}
+      >
+        {sortedData.map((group, groupIndex) => {
+          return (
+            <div
+              key={groupIndex}
+              style={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                border: "1px solid black",
+              }}
+            >
+              {group.map((entry, index) => {
+                return (
+                  <DataEntry
+                    key={entry.id}
+                    url={entry.url}
+                    name={entry.name}
+                    size={150}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
