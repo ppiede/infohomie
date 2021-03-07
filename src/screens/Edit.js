2a112ebb7a0599a2f49373e4d90b8d69123447bf
lkoehl <48966896+lkoehl@users.noreply.github.com>
2021-03-07 14:46:06 +0100

    Change text in Edit

--- src/screens/Edit.js
+++ src/screens/Edit.js
@@ -3,7 +3,10 @@ import { React } from "react";
 const Help = () => {
   return (
     <div>
-      <p>Hier ist Platz, um einen bestehenden Baum zu bearbeiten</p>
+      <p>
+        Hier ist Platz, um einen bestehenden Datensatz zu bearbeiten oder einen
+        neuen Datensatz zu erstellen
+      </p>
     </div>
   );
 };
