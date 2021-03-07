233d9c2358174bbead50841589ff34e35b23200c
lkoehl <48966896+lkoehl@users.noreply.github.com>
2021-03-07 18:01:41 +0100

    Tabelle klappt

--- src/screens/Edit.js
+++ src/screens/Edit.js
@@ -2,18 +2,16 @@ import { React, useState, useMemo, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useDropzone } from "react-dropzone";
 import DataEntry from "../components/DataEntry";
+import Table from "../components/Table";
 
 const Edit = () => {
   const query = new URLSearchParams(useLocation().search);
   const mode = query.get("mode");
   const [files, setFiles] = useState([]);
+  const [featureName, setFeatureName] = useState("");
+  const [features, setFeatures] = useState(["ears", "eyes", "hair"]);
 
-  const {
-    acceptedFiles,
-    getRootProps,
-    getInputProps,
-    isDragActive,
-  } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: "image/*",
     onDrop: (acceptedFiles) => {
       setFiles(
@@ -74,6 +72,53 @@ const Edit = () => {
     [isDragActive]
   );
 
+  const makeColumns = () => {
+    const columns = [];
+    columns.push({
+      Header: "Name",
+      accessor: "name",
+    });
+
+    for (let i = 0; i < features.length; i++) {
+      columns.push({
+        Header: features[i],
+        accessor: features[i],
+      });
+    }
+
+    return columns;
+  };
+
+  const columns = useMemo(() => makeColumns(), [features, files]);
+
+  const makeData = () => {
+    const data = [];
+
+    for (let i = 0; i < files.length; i++) {
+      let obj = { name: files[i].name };
+      for (let j = 0; j < features.length; j++) {
+        obj[features[j]] = true;
+      }
+
+      data.push(obj);
+    }
+
+    return data;
+  };
+
+  const [data, setData] = useState(makeData());
+
+  useEffect(() => {
+    setData(makeData);
+  }, [files, features]);
+
+  const handleClick = () => {
+    let copy = [...features];
+    copy.push(featureName);
+    setFeatures(copy);
+    setFeatureName("");
+  };
+
   return (
     <div
       style={{
@@ -103,7 +148,14 @@ const Edit = () => {
         </p>
       )}
       {files.length !== 0 ? (
-        <div style={{ marginTop: 32 }}>
+        <div
+          style={{
+            marginTop: 32,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <p>Diese Fotos wurden von dir hochgeladen</p>
           <div
             style={{
@@ -113,6 +165,24 @@ const Edit = () => {
             }}
           >
             {acceptedFileImages}
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", width: 200 }}>
+            <input
+              type="text"
+              value={featureName}
+              onChange={(event) => setFeatureName(event.target.value)}
+            />
+            <button onClick={handleClick}>Neue Spalte hinzufügen</button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              alignItems: "stretch",
+            }}
+          >
+            <Table columns={columns} data={data} />
           </div>
         </div>
       ) : (
