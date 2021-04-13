b369be626e24604d014b35f7f299fad340eda7d9
lkoehl <48966896+lkoehl@users.noreply.github.com>
2021-04-13 09:41:03 +0200

    Change DBConfig for multistore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import { React } from "react";
 import Router from "./router";
+import { DBConfig } from "./DBConfig";
+import { initDB } from "react-indexed-db";
 
+initDB(DBConfig(["tiere", "gurken", "menschen"]));
 function App() {
   return (
     <div className="App">
--- src/DBConfig.js
+++ src/DBConfig.js
@@ -1,14 +1,63 @@
-export const DBConfig = {
-    name: 'ImageDB',
+export const DBConfig = (stores) => {
+  // Funktioniert nicht, keine Ahnung wieso. Der Output sieht richtig aus
+  /*
+  const storeList = [];
+  for (let i = 0; i < stores.length; i++) {
+    storeList.push({
+      store: stores[i],
+      storeConfig: { keyPath: "id", autoIncrement: true },
+      storeSchema: [
+        { name: "name", keypath: "name", options: { unique: false } },
+        {
+          name: "binarydata",
+          keypath: "binarydata",
+          options: { unique: false },
+        },
+      ],
+    });
+  }
+  console.log("list", storeList);
+
+  const config = {
+    name: "ImageDB",
+    version: 1,
+    objectStoresMeta: [storeList],
+  };
+
+  console.log("config", config);
+
+  return config;
+*/
+
+  // Funktioniert scheinbar laut Google Dev Tools
+  return {
+    name: "ImageDB",
     version: 1,
     objectStoresMeta: [
       {
-        store: 'people',
-        storeConfig: { keyPath: 'id', autoIncrement: true },
+        store: "tiere",
+        storeConfig: { keyPath: "id", autoIncrement: true },
         storeSchema: [
-          { name: 'name', keypath: 'name', options: { unique: false } },
-          { name: 'binarydata', keypath: 'binarydata', options: { unique: false } }
-        ]
-      }
-    ]
+          { name: "name", keypath: "name", options: { unique: false } },
+          {
+            name: "binarydata",
+            keypath: "binarydata",
+            options: { unique: false },
+          },
+        ],
+      },
+      {
+        store: "gurken",
+        storeConfig: { keyPath: "id", autoIncrement: true },
+        storeSchema: [
+          { name: "name", keypath: "name", options: { unique: false } },
+          {
+            name: "binarydata",
+            keypath: "binarydata",
+            options: { unique: false },
+          },
+        ],
+      },
+    ],
   };
+};
