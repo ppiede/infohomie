da859cdc928334ab0b507907a863da31ea21066a
lkoehl <48966896+lkoehl@users.noreply.github.com>
2021-04-15 09:24:27 +0200

    Dropdown SelectTreeDataset funktioniert

--- src/screens/DecisionTree.js
+++ src/screens/DecisionTree.js
@@ -2,129 +2,159 @@ import { React, useState, useMemo } from "react";
 import { getFeatures, GetDataset } from "../mock";
 import DataEntry from "../components/DataEntry";
 import DecisionTreeVisualizer from "../decision-tree-visualizer";
-import {Link, useLocation} from "react-router-dom";
-import Logo from '../img/YouChooseLogo.png';
+import { useLocation } from "react-router-dom";
 import Footer from "../components/Footer.js";
-import {DropdownButton, Dropdown} from 'react-bootstrap';
-
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const DecisionTree = () => {
+  let page = [];
 
-    let page = [];
+  const query = new URLSearchParams(useLocation().search);
+  const datasetID = query.get("id");
 
+  const dataset = GetDataset(datasetID);
+  console.log("dataset");
+  console.log(dataset);
 
-    const query = new URLSearchParams(useLocation().search);
-    const datasetID = query.get("id");
+  const features = useMemo(() => getFeatures(datasetID), []);
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
 
+  const handleChange = (event) => {
+    let copy = [...selectedFeatures];
+    copy[event.target.id] = event.target.value;
+    setSelectedFeatures(copy);
+  };
 
+  const renderData = () => {
+    return dataset.map((value, index) => {
+      return (
+        <DataEntry key={index} url={value.url} name={value.name} size={150} />
+      );
+    });
+  };
 
-    const dataset = GetDataset(datasetID);
-    console.log("dataset");
-    console.log(dataset);
-
-    
-    const features = useMemo(() => getFeatures(datasetID), []);
-    const [selectedFeatures, setSelectedFeatures] = useState([]);
-
-      const handleChange = (event) => {
-        let copy = [...selectedFeatures];
-        copy[event.target.id] = event.target.value;
-        setSelectedFeatures(copy);
-      };
-
-      const renderData = () => {
-        return dataset.map((value, index) => {
-          return (
-            <DataEntry key={index} url={value.url} name={value.name} size={150} />
-          );
-        });
-      };
-
-    const body = (
-    
+  const body = (
     <div>
+      <br />
 
+      <p>Unsere Tierpension behaust aktuell folgende Tiere:</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {renderData()}
+      </div>
+      <p>
+        Jetzt wollen wir unsere Hunde von unseren Katzen trennen und müssen
+        dafür Kriterien festlegen:
+      </p>
+      <table class="center">
+        <td class="center">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Kriterium 1 auswählen"
+            value={selectedFeatures[0]}
+            id={0}
+            onSelect={handleChange}
+          >
+            {Object.keys(features).map((key, index) => {
+              return (
+                <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+        </td>
+        <td class="center">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Kriterium 2 auswählen"
+            value={selectedFeatures[1]}
+            id={1}
+            onSelect={handleChange}
+          >
+            {Object.keys(features).map((key, index) => {
+              return (
+                <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+        </td>
+        <td class="center">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Kriterium 3 auswählen"
+            value={selectedFeatures[2]}
+            id={2}
+            onSelect={handleChange}
+          >
+            {Object.keys(features).map((key, index) => {
+              return (
+                <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+        </td>
+        <td class="center">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Kriterium 4 auswählen"
+            value={selectedFeatures[3]}
+            id={3}
+            onSelect={handleChange}
+          >
+            {Object.keys(features).map((key, index) => {
+              return (
+                <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+        </td>
+      </table>
+
+      <select value={selectedFeatures[0]} id={0} onChange={handleChange}>
+        <option value="">{"Kriterium 1 auswählen"}</option>
+        {Object.keys(features).map((key, index) => {
+          return <option value={key}>{features[key].label}</option>;
+        })}
+      </select>
+      <select value={selectedFeatures[1]} id={1} onChange={handleChange}>
+        <option value="">{"Kriterium 2 auswählen"}</option>
+        {Object.keys(features).map((key, index) => {
+          return <option value={key}>{features[key].label}</option>;
+        })}
+      </select>
+      <select value={selectedFeatures[2]} id={2} onChange={handleChange}>
+        <option value="">{"Kriterium 3 auswählen"}</option>
+        {Object.keys(features).map((key, index) => {
+          return <option value={key}>{features[key].label}</option>;
+        })}
+      </select>
+      <select value={selectedFeatures[3]} id={3} onChange={handleChange}>
+        <option value="">{"Kriterium 4 auswählen"}</option>
+        {Object.keys(features).map((key, index) => {
+          return <option value={key}>{features[key].label}</option>;
+        })}
+      </select>
+      <DecisionTreeVisualizer
+        dataset={dataset}
+        features={features}
+        selectedFeatures={selectedFeatures}
+      />
+      <div style={{ bottom: "0", width: "100%" }}>
         <br />
+        <Footer />
+      </div>
+    </div>
+  );
 
-        <p>Unsere Tierpension behaust aktuell folgende Tiere:</p>
-        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center" }}>
-            {renderData()}
-        </div>
-        <p>
-            Jetzt wollen wir unsere Hunde von unseren Katzen trennen und müssen
-            dafür Kriterien festlegen:
-        </p>
-        <table class="center">
-            <td class="center">
-                <DropdownButton id="dropdown-basic-button" title="Kriterium 1 auswählen" value={selectedFeatures[0]} id={0} onSelect={handleChange}>
-                    {Object.keys(features).map((key, index) => {
-                        return <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>;
-                    })}
-                </DropdownButton>
-            </td>
-            <td class="center">
-                <DropdownButton id="dropdown-basic-button" title="Kriterium 2 auswählen" value={selectedFeatures[1]} id={1} onSelect={handleChange}>
-                    {Object.keys(features).map((key, index) => {
-                        return <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>;
-                    })}
-                </DropdownButton>
-            </td>
-            <td class="center">
-                <DropdownButton id="dropdown-basic-button" title="Kriterium 3 auswählen" value={selectedFeatures[2]} id={2} onSelect={handleChange}>
-                    {Object.keys(features).map((key, index) => {
-                        return <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>;
-                    })}
-                </DropdownButton>
-            </td>
-            <td class="center">
-                <DropdownButton id="dropdown-basic-button" title="Kriterium 4 auswählen" value={selectedFeatures[3]} id={3} onSelect={handleChange}>
-                    {Object.keys(features).map((key, index) => {
-                        return <Dropdown.Item value={key}>{features[key].label}</Dropdown.Item>;
-                    })}
-                </DropdownButton>
-            </td>
-        </table>
-        
-        <select value={selectedFeatures[0]} id={0} onChange={handleChange}>
-            <option value="">{"Kriterium 1 auswählen"}</option>
-            {Object.keys(features).map((key, index) => {
-                return <option value={key}>{features[key].label}</option>;
-            })}
-        </select>
-        <select value={selectedFeatures[1]} id={1} onChange={handleChange}>
-            <option value="">{"Kriterium 2 auswählen"}</option>
-            {Object.keys(features).map((key, index) => {
-                return <option value={key}>{features[key].label}</option>;
-            })}
-        </select>
-        <select value={selectedFeatures[2]} id={2} onChange={handleChange}>
-            <option value="">{"Kriterium 3 auswählen"}</option>
-            {Object.keys(features).map((key, index) => {
-                return <option value={key}>{features[key].label}</option>;
-            })}
-        </select>
-        <select value={selectedFeatures[3]} id={3} onChange={handleChange}>
-            <option value="">{"Kriterium 4 auswählen"}</option>
-            {Object.keys(features).map((key, index) => {
-                return <option value={key}>{features[key].label}</option>;
-            })}
-        </select>
-        <DecisionTreeVisualizer
-            dataset={dataset}
-            features={features}
-            selectedFeatures={selectedFeatures}
-        />
-        <div style={{bottom: "0", width: "100%"}}>
-            <br />
-            <Footer />
-        </div>
-    </div>)
+  page.push(body);
+  //page.push(GetAll(datasetID));
 
-
-    page.push(body);
-    //page.push(GetAll(datasetID));
-
-    return page;
+  return page;
 };
 
 export default DecisionTree;
--- src/screens/SelectTreeDataset.js
+++ src/screens/SelectTreeDataset.js
@@ -1,22 +1,14 @@
-import React, { Component, useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import { getDatasets } from "../mock";
-import DecisionTree from "./DecisionTree";
-import DecisionTreeVisualizer from "../decision-tree-visualizer";
-import { BrowserRouter, Route, withRouter } from "react-router-dom";
-import { useIndexedDB } from 'react-indexed-db';
-import ls from 'local-storage';
-import Logo from '../img/YouChooseLogo.png';
+import { withRouter } from "react-router-dom";
+import Logo from "../img/YouChooseLogo.png";
 import Footer from "../components/Footer.js";
-import { DropdownButton, Dropdown } from 'react-bootstrap';
-
-
-
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const SelectTreeDataset = () => {
-    let html = [];
+  let html = [];
 
-    /*
+  /*
     let datasetList = ls.get('datasetList');
     if(datasetList == null ){
         /*
@@ -47,60 +39,63 @@ const SelectTreeDataset = () => {
         dropDownOptions.push(datasetList[i]['name']);
     }
     */
-    let dropDownOptions = getDatasets();
+  let dropDownOptions = getDatasets();
 
-
-
-    class dropDownMenu extends Component {
-        handleChange = (event) => {
-            this.props.history.push("/decision-tree?id=" + dropDownOptions[event.target.value]);
-        };
-       render() {
-           return(
-               <div>
-                   <img
-                    src={Logo}
-                    height="150"
-                    className="d-inline-block align-top"
-                    alt="You choose Logo"
-                    />
-                <div style={{
-                    margin: "0",
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                }} >
-                    <p>
-                        Hier können Sie einen schon existierenden Datensatz auswählen:
-                    </p>
-                    <DropdownButton id="dropdown-basic-button" title="Datensatz auswählen" value={dropDownOptions} id={0} onSelect={this.handleChange}>
-                        {Object.keys(dropDownOptions).map((key, index) => {
-                            return <Dropdown.Item>{dropDownOptions[key]}</Dropdown.Item>;
-                        })}
-                    </DropdownButton>
-                    
-                    <select value={dropDownOptions} id={0} onChange={this.handleChange}>
-                        <option value="">{"Datensatz auswählen"}</option>
-                        {Object.keys(dropDownOptions).map((key, index) => {
-                            return <option value={key}>{dropDownOptions[key]}</option>;
-                        })}
-                    </select>
-                    <br />
-                    
-                </div>
-                <div style={{position: "absolute", bottom: "0", width: "100%"}}>
-                        <Footer />
-                </div>
-                </div>
-            );
-        }
+  class dropDownMenu extends Component {
+    handleChange = (event) => {
+      console.log(event);
+      this.props.history.push("/decision-tree?id=" + dropDownOptions[event]);
+    };
+    render() {
+      console.log(dropDownOptions);
+      return (
+        <div>
+          <img
+            src={Logo}
+            height="150"
+            className="d-inline-block align-top"
+            alt="You choose Logo"
+          />
+          <div
+            style={{
+              margin: "0",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <p>
+              Hier können Sie einen schon existierenden Datensatz auswählen:
+            </p>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Datensatz auswählen"
+              value={dropDownOptions}
+              id={0}
+            >
+              {Object.keys(dropDownOptions).map((key, index) => {
+                return (
+                  <Dropdown.Item eventKey={key} onSelect={this.handleChange}>
+                    {dropDownOptions[key]}
+                  </Dropdown.Item>
+                );
+              })}
+            </DropdownButton>
+            <br />
+          </div>
+          <div style={{ position: "absolute", bottom: "0", width: "100%" }}>
+            <Footer />
+          </div>
+        </div>
+      );
     }
+  }
 
-    const Menu = withRouter(dropDownMenu);
+  const Menu = withRouter(dropDownMenu);
 
-    html.push(<Menu />)
-    return html;
+  html.push(<Menu />);
+  return html;
 };
 
 export default SelectTreeDataset;
